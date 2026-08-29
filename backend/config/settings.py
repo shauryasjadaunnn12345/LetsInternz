cbb15093b7e49908fc77017245a9995e4ce7afea
shaurya singh jadaun <shauryasjadaunnn@gmail.com>
2026-08-29 19:16:35 +0530

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-change-me-in-production")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env_bool("DEBUG", True)
 
-ALLOWED_HOSTS = env_list("ALLOWED_HOSTS", "localhost,127.0.0.1")
+ALLOWED_HOSTS = env_list("ALLOWED_HOSTS", "localhost,127.0.0.1","*")
 
 
 # Application definition
